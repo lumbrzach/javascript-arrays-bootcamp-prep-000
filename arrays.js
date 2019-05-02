bb2a6a43392c445b574f7b13f602cdebc610c3bf
Zach Parker <parkerz410@gmail.com>
2019-05-02 21:38:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,11 @@ function removeElementFromBeginningOfArray (chocolateBars) {
   return chocolateBars.slice(1);
 };
 
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+};
+
 function removeElementFromEndOfArray(chocolateBars){
   return chocolateBars.slice(0, chocolateBars.length - 1);
 };
